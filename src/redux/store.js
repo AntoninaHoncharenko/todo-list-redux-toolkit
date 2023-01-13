040846c9a7a72a1ca3d4df438a702a163da16073
AntoninaHoncharenko <g.antonina.vl@gmail.com>
2023-01-13 20:09:53 +0200

    add persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,31 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { taskReducer } from './taskSlice';
 import { filterReducer } from './filterSlice';
 
+const persistTaskConfig = {
+  key: 'tasks',
+  storage,
+};
+
+const persistFilterConfig = {
+  key: 'filters',
+  storage,
+};
+
+const persistedTaskReducer = persistReducer(persistTaskConfig, taskReducer);
+
+const persistedFilterReducer = persistReducer(
+  persistFilterConfig,
+  filterReducer
+);
+
 export const store = configureStore({
   reducer: {
-    tasks: taskReducer,
-    filters: filterReducer,
+    tasks: persistedTaskReducer,
+    filters: persistedFilterReducer,
   },
 });
+
+export const persistor = persistStore(store);
