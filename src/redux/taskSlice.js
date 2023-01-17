ac5389f7f1217db4c23222188bfc058cd0756f96
AntoninaHoncharenko <g.antonina.vl@gmail.com>
2023-01-17 11:15:13 +0200

    Update taskSlice.js

--- src/redux/taskSlice.js
+++ src/redux/taskSlice.js
@@ -11,40 +11,50 @@ const extraActions = [fetchTasks, addTask, deleteTask, toggleCompleted];
 
 const getActions = type => isAnyOf(...extraActions.map(action => action[type]));
 
+const fetchTasksReducer = (state, action) => {
+  state.tasks = action.payload;
+};
+
+const addTaskReducer = (state, action) => {
+  state.tasks.push(action.payload);
+};
+
+const deleteTaskReducer = (state, action) => {
+  const index = state.tasks.findIndex(task => task.id === action.payload.id);
+  state.tasks.splice(index, 1);
+};
+
+const toggleCompletedReducer = (state, action) => {
+  const index = state.tasks.findIndex(task => task.id === action.payload.id);
+  state.tasks.splice(index, 1, action.payload);
+};
+
+const pendingReducer = state => {
+  state.isLoading = true;
+};
+
+const rejectedReducer = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
+const fulfilledReducer = state => {
+  state.isLoading = false;
+  state.error = null;
+};
+
 const tasksSlice = createSlice({
   name: 'tasks',
   initialState: taskInitialState,
   extraReducers: builder =>
     builder
-      .addCase(fetchTasks.fulfilled, (state, action) => {
-        state.tasks = action.payload;
-      })
-      .addCase(addTask.fulfilled, (state, action) => {
-        state.tasks.push(action.payload);
-      })
-      .addCase(deleteTask.fulfilled, (state, action) => {
-        const index = state.tasks.findIndex(
-          task => task.id === action.payload.id
-        );
-        state.tasks.splice(index, 1);
-      })
-      .addCase(toggleCompleted.fulfilled, (state, action) => {
-        const index = state.tasks.findIndex(
-          task => task.id === action.payload.id
-        );
-        state.tasks.splice(index, 1, action.payload);
-      })
-      .addMatcher(getActions('pending'), state => {
-        state.isLoading = true;
-      })
-      .addMatcher(getActions('rejected'), (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
-      })
-      .addMatcher(getActions('fulfilled'), state => {
-        state.isLoading = false;
-        state.error = null;
-      }),
+      .addCase(fetchTasks.fulfilled, fetchTasksReducer)
+      .addCase(addTask.fulfilled, addTaskReducer)
+      .addCase(deleteTask.fulfilled, deleteTaskReducer)
+      .addCase(toggleCompleted.fulfilled, toggleCompletedReducer)
+      .addMatcher(getActions('pending'), pendingReducer)
+      .addMatcher(getActions('rejected'), rejectedReducer)
+      .addMatcher(getActions('fulfilled'), fulfilledReducer),
 });
 
 export const tasksReducer = tasksSlice.reducer;
